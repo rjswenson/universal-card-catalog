90fb73b8a98720afc37f215e7e49ccf43a172a54
Robin Swenson-Healey <rjswenson@gmail.com>
2016-04-21 17:00:33 -0600

    stub unit controller

---
+++ api/controllers/UnitController.js
@@ -0,0 +1,11 @@
+/**
+ * UnitController
+ *
+ * @description :: Server-side logic for managing units
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
